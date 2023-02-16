d9b80239a5ad0e62220aaa36df8a424c2f6af063
Adam <tommaso140413@gmail.com>
2023-02-16 16:08:07 +0300

    Add lesson03 Big O Notation and theory file

--- src/Lesson01_FibonacciNumber/Fibonacci.java
+++ src/Lesson01_FibonacciNumber/Fibonacci.java
@@ -1,13 +1,23 @@
 package Lesson01_FibonacciNumber;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Fibonacci {
     public static void main(String[] args) {
-        System.out.println(fibNums(100));
+        Scanner scan = new Scanner(System.in);
+        int userNum = scan.nextInt();
+
+        long[] mem = new long[userNum + 1]; // массив для мемоизации, чтобы запоминать повторы для error метода
+        Arrays.fill(mem, -1); // метод который заполняет массив одинаковыми значениями
+        System.out.println(error(userNum, mem));
     }
 
-    // правильный метод
-    private static long fibNums (int n) {
+    // правильный метод с оценкой O(n)
+    private static long fibNums(int n) {
         long[] nums = new long[n + 1];
+
+        // по условию первое и второе число равны 0 и 1
         nums[0] = 0;
         nums[1] = 1;
 
@@ -17,11 +27,21 @@ public class Fibonacci {
         return nums[n];
     }
 
-    // неправильный метод через рекурсию
-    private static long error(int n) {
+    // неправильный метод через рекурсию, который можно исправить с помощью мемоизации
+    // мемоизация - сохранение результатов выполнения функций для предотвращения повторных вычислений (Lesson02)
+    // без мемоизации сложность алгоритма составляет O(2^n)
+    private static long error(int n, long[] mem) {
+        // условие исключающее повторные вычисления
+        if(mem[n] != 1) {
+            return mem[n];
+        }
+
         if (n <= 1) {
             return n;
         }
-        return error(n - 1) + error(n - 2);
+
+        long result = error(n - 1, mem) + error(n - 2, mem);
+        mem[n] = result;
+        return result;
     }
 }
